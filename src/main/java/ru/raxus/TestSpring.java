3855beaa3cd56da254b0d1f2201d62616baf9939
Raxus109 <mikhalin91@gmail.com>
2021-11-28 16:48:36 +0300

    com

--- src/main/java/ru/raxus/TestSpring.java
+++ src/main/java/ru/raxus/TestSpring.java
@@ -8,10 +8,20 @@ public class TestSpring {
         //Music music = context.getBean("musicBean", Music.class);//указывается id из application context и объект класса, который хотим получить
 
         //указывается id из application context и класс, объект которого хотим получить
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        boolean b = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(b);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+        System.out.println("Первый проигрыватель: " + firstMusicPlayer.getVolume());
+        System.out.println("Второй проигрыватель: " + secondMusicPlayer.getVolume());
+        firstMusicPlayer.setVolume(10);
+        System.out.println("Первый проигрыватель: " + firstMusicPlayer.getVolume());
+        System.out.println("Второй проигрыватель: " + secondMusicPlayer.getVolume());
+        //musicPlayer.playMusicList();
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
